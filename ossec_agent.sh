4acf71aa39356bae1d6cd437457cd01e1568b432
mmalamud <mmalamud@2tor.com>
2015-05-12 20:41:06 +0300

    Update ossec_agent.sh

--- ossec_agent.sh
+++ ossec_agent.sh
@@ -11,6 +11,7 @@ HOSTNAME=$2
 }
 
 [ -f /usr/bin/aptitude ] && aptitude -y install git libssl-dev wget
+[ -f /usr/bin/apt-get ] && apt-get -y install git libssl-dev wget
 [ -f /usr/bin/yum ] && yum -y install git openssl-devel wget
 
 [ ! -f /usr/bin/chef-solo ] && curl -LO https://www.chef.io/chef/install.sh && sudo bash ./install.sh -v 11.18.6 && rm install.sh
