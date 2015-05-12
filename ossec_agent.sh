835162e1277c24a39561b74785c8af8d0922b139
mmalamud <mmalamud@2tor.com>
2015-05-12 17:59:13 +0300

    Use instance id /hostname

--- ossec_agent.sh
+++ ossec_agent.sh
@@ -4,7 +4,14 @@ SERVER=$1
 HOSTNAME=$2
 
 [ "$SERVER" = "" ] && echo "Usage: $0 ServerName AgentName" && exit 1
-[ "$HOSTNAME" = "" ] && echo "Usage: $0 ServerName AgentName" && exit 1
+[ "$HOSTNAME" = "" ] && { 
+ echo "Using instance ID if present or actual hostname if not as agent name"
+ HOSTNAME=`wget -qO- http://169.254.169.254/latest/meta-data/instance-id`
+ [ "$HOSTNAME" = "" ] && HOSTNAME=`hostname`
+}
+
+echo $HOSTNAME
+exit 1
 
 [ ! -f /usr/bin/chef-solo ] && curl -LO https://www.chef.io/chef/install.sh && sudo bash ./install.sh -v 11.18.6 && rm install.sh
 
